7d87137d8d7372eebc38f52d220301105377755a
liwz <liwz0881@fingard.com>
2020-09-29 17:34:33 +0800

    gateway

---
+++ gateway/src/main/java/xyz/lwz/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package xyz.lwz.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author liwz
+ * @Date 2020/9/29 17:09
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
