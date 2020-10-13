3f612b43969c959d3013a8c19a54aabdbb3a3bee
liwz <liwz0881@fingard.com>
2020-10-13 18:00:46 +0800

    update

--- dubbo-provider/src/main/java/xyz/lwz/provider/ProviderApplication.java
+++ dubbo-provider/src/main/java/xyz/lwz/provider/ProviderApplication.java
@@ -2,12 +2,14 @@ package xyz.lwz.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author liwz
  * @Date 2020/9/30 10:19
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProviderApplication {
 
     public static void main(String[] args) {
