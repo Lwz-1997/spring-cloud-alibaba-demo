25cb9f86850f8c25e6c305b12c004da31bbc804b
lwz <lwz_1997@outlook.com>
2020-10-15 00:02:10 +0800

    mybatis-plus-generator

---
+++ mybatis-plus-generator/src/main/java/xyz/lwz/CodeGenerator.java
@@ -0,0 +1,195 @@
+package xyz.lwz;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+/**
+ * 运行此方法生成mybatis代码
+ * 生成代码自动放入对应目录
+ *
+ * @author liwz
+ * @date 2020/10/14 22:40
+ */
+public class CodeGenerator {
+
+    /**
+     * 数据库类型
+     */
+    private static final DbType DB_TYPE = DbType.MYSQL;
+
+    /**
+     * 数据库连接信息
+     */
+    private static final String DB_URL = "jdbc:mysql://47.100.185.35:3306/blog?useUnicode=true&characterEncoding=utf-8&serverTimezone=Asia/Shanghai&useSSL=false&allowPublicKeyRetrieval=true" ;
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver" ;
+    private static final String USERNAME = "root" ;
+    private static final String PASSWORD = "dytqce94lwz" ;
+
+    /**
+     * 项目地址
+     */
+    private static final String PROJECT_PATH = "E:/code-github/study/" ;
+
+    /**
+     * 项目名
+     */
+    private static final String PROJECT_NAME = "spring-cloud-alibaba-demo/mybatis-plus-generator" ;
+
+    /**
+     * 指定包名
+     */
+    private static final String PACKAGE_NAME = "xyz.lwz" ;
+
+    /**
+     * controller基础类
+     */
+    private static final String SUPER_CONTROLLER_CLASS = PACKAGE_NAME + ".common.BaseController" ;
+
+    /**
+     * entity基础类
+     */
+    private static final String SUPER_ENTITY_CLASS = PACKAGE_NAME + ".common.BaseEntity" ;
+
+    /**
+     * 作者名
+     */
+    private static final String AUTHOR = "liwz" ;
+
+    /**
+     * 指定生成的表名
+     */
+    private static final String[] TABLE_NAMES = new String[]{"user"};
+
+    public static void main(String[] args) {
+        // serviceNameStartWithI：user -> UserService, 设置成true: user -> IUserService
+        new CodeGenerator().generateByTables(TABLE_NAMES);
+    }
+
+    /**
+     * 根据表自动生成
+     *
+     * @param tableNames 表名
+     * @author Terry
+     */
+    private void generateByTables(String... tableNames) {
+        // 配置数据源
+        DataSourceConfig dataSourceConfig = getDataSourceConfig();
+        // 策略配置
+        StrategyConfig strategyConfig = getStrategyConfig(tableNames);
+        // 全局变量配置
+        GlobalConfig globalConfig = getGlobalConfig();
+        // 包名配置
+        PackageConfig packageConfig = getPackageConfig();
+        // 自动生成
+        autoGenerator(dataSourceConfig, strategyConfig, globalConfig, packageConfig);
+    }
+
+    /**
+     * 集成
+     *
+     * @param dataSourceConfig 配置数据源
+     * @param strategyConfig   策略配置
+     * @param config           全局变量配置
+     * @param packageConfig    包名配置
+     * @author Terry
+     */
+    private void autoGenerator(DataSourceConfig dataSourceConfig, StrategyConfig strategyConfig, GlobalConfig config, PackageConfig packageConfig) {
+        new AutoGenerator()
+                .setGlobalConfig(config)
+                .setDataSource(dataSourceConfig)
+                .setStrategy(strategyConfig)
+                .setPackageInfo(packageConfig)
+                .setTemplateEngine(new FreemarkerTemplateEngine())
+                .execute();
+    }
+
+    /**
+     * 设置包名
+     *
+     * @return PackageConfig 包名配置
+     * @author Terry
+     */
+    private PackageConfig getPackageConfig() {
+        return new PackageConfig()
+                .setParent(PACKAGE_NAME)
+                .setXml("mapper.xml" )
+                .setMapper("mapper" )
+                .setController("controller" )
+                .setEntity("entity" );
+    }
+
+    /**
+     * 全局配置
+     *
+     * @return GlobalConfig
+     * @author Terry
+     */
+    private GlobalConfig getGlobalConfig() {
+        GlobalConfig globalConfig = new GlobalConfig();
+        globalConfig
+                .setBaseColumnList(true)
+                .setBaseResultMap(true)
+                .setActiveRecord(false)
+                //作者
+                .setAuthor(AUTHOR)
+                //设置输出路径
+                .setOutputDir(getOutputDir())
+                .setFileOverride(true);
+        //设置service名
+        globalConfig.setServiceName("%sService" );
+        return globalConfig;
+    }
+
+    /**
+     * 返回项目路径
+     *
+     * @return 项目路径
+     * @author Terry
+     */
+    private String getOutputDir() {
+        return PROJECT_PATH + PROJECT_NAME + "/src/main/java/" ;
+    }
+
+    /**
+     * 策略配置
+     *
+     * @param tableNames 表名
+     * @return StrategyConfig
+     * @author Terry
+     */
+    private StrategyConfig getStrategyConfig(String... tableNames) {
+        return new StrategyConfig()
+                // 全局大写命名 ORACLE 注意
+                .setCapitalMode(true)
+                // 从数据库表到文件的命名策略
+                .setNaming(NamingStrategy.underline_to_camel)
+                // 需要生成的的表名，多个表名传数组
+                .setInclude(tableNames)
+                // 公共父类
+                .setSuperControllerClass(SUPER_CONTROLLER_CLASS)
+                .setSuperEntityClass(SUPER_ENTITY_CLASS)
+                // 写于父类中的公共字段
+                .setSuperEntityColumns("id" )
+                // 使用lombok
+                .setEntityLombokModel(true)
+                // rest风格
+                .setRestControllerStyle(true);
+    }
+
+    /**
+     * 配置数据源
+     *
+     * @return 数据源配置 DataSourceConfig
+     * @author Terry
+     */
+    private DataSourceConfig getDataSourceConfig() {
+        return new DataSourceConfig().setDbType(DB_TYPE)
+                .setUrl(DB_URL)
+                .setUsername(USERNAME)
+                .setPassword(PASSWORD)
+                .setDriverName(DRIVER);
+    }
+}
