c1c7717a28ede1a494d8c4d6851e69d108ebb35e
liwz <liwz0881@fingard.com>
2021-01-28 15:51:10 +0800

    修改

---
+++ .history/dubbo-api/src/main/java/xyz/lwz/provider/api/HelloApi_20210128154457.java
@@ -0,0 +1,15 @@
+package xyz.lwz.provider.api;
+
+/**
+ * dada
+ * @Author liwz
+ * @Date 2020/9/30 10:15
+ **/
+public interface HelloApi {
+
+    /**
+     *
+     * @return String
+     */
+    String hello();
+}
